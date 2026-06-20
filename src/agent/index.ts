dc58232c966f0d6497bf0de1941f101954f9f1db
funclosure <funclosure@gmail.com>
2026-06-20 21:09:49 +0800

    feat: agent barrel + root re-export

---
+++ src/agent/index.ts
@@ -0,0 +1,6 @@
+export * from "./model-client";
+export * from "./prompts";
+export * from "./json";
+export * from "./prompter";
+export * from "./ingest";
+export { anthropicClient } from "./anthropic-client";
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./outline/index";
 export * from "./render/index";
 export * from "./export/index";
+export * from "./agent/index";
